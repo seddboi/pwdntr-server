2183b472d6f307896e1e60f165c28e88946335ba
Gian Zamora <gz2016@att.net>
2023-11-20 22:00:06 -0800

    added delete method to allowed methods

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use((req, res, next) => {
 	// res.header('Access-Control-Allow-Origin', 'https://passwordinator.netlify.app');
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT, DELETE');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
